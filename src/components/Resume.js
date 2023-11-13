0f0c06aaab5331be91e317fdc02aa9b3cdd29ee0
Wilson Zimthamaha Bonkuru <wilsonzim566@gmail.com>
2023-11-14 00:57:47 +0100

    done

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import CV from "../data/Wilson Zimthamaha-CV.pdf";
+import CV from "../data/CV.pdf";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
